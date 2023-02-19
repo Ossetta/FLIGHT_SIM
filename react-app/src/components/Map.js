82a774c926d999ec08d657a67843d2b9f7b0e83b
Jeff Castro <116311056+Jcastro197@users.noreply.github.com>
2023-02-19 12:03:40 -0500

    Erased table

--- react-app/src/components/Map.js
+++ react-app/src/components/Map.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import DrawingCanvas from './DrawingCanvas/DrawingCanvas'
+import './Map.css'
 
 const Map = () => {
+  let gridLines = [];
+  for (let i = 0; i < 9 * 9; i++) {
+    gridLines.push(<div className="grid-line"></div>);
+  }
+
   return (
     <div className='map'>
       <img src='./images/map.png' alt='map'/>
+      <div className = 'gridilines'> </div>
       <DrawingCanvas />
     </div>
   )
 }
 
 export default Map
+
+
+
--- react-app/src/components/PlaneTable.js
+++ react-app/src/components/PlaneTable.js
@@ -2,19 +2,21 @@ import React from 'react'
 
 const PlaneTable = () => {
   return (
-    <div className='table1'>
-        <table>
-            <thead>
-                <td>
-                    <th>Planes:</th>
-                        <tr>Plane1</tr>
-                        <tr>Plane2</tr>
-                        <tr>Plane3</tr>
-                </td>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
+    <div className='plane-table'>
+      <div className='plane-table__header'>
+        <div className='plane-table__header__item'>Plane</div>
+        <div className='plane-table__header__item'>Origin</div>
+        <div className='plane-table__header__item'>Destination</div>
+        <div className='plane-table__header__item'>Departure</div>
+        <div className='plane-table__header__item'>Arrival</div>
+      </div>
+      <div className='plane-table__body'>
+        <div className='plane-table__body__item'>Plane</div>
+        <div className='plane-table__body__item'>Origin</div>
+        <div className='plane-table__body__item'>Destination</div>
+        <div className='plane-table__body__item'>Departure</div>
+        <div className='plane-table__body__item'>Arrival</div>
+      </div>
     </div>
   )
 }
