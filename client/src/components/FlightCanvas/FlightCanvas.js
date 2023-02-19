f1885716f307240064aebd8eaccdc40cbb2125a5
Jeff Castro <116311056+Jcastro197@users.noreply.github.com>
2023-02-19 15:05:11 -0500

    Co-authored-by: ChefOmarDee <ChefOmarDee@users.noreply.github.com>

---
+++ client/src/components/FlightCanvas/FlightCanvas.js
@@ -0,0 +1,15 @@
+import map from './map.webp'
+import './FlightCanvas.css'
+//import React from 'react'
+
+let FlightCanvas=()=>{
+
+    return(
+        // <canvas style={{ backgroundImage: `url(${map})`, width:"500px", height:"500px"}}></canvas>  
+        //  <canvas style={{ backgroundImage: `url(${map})`,  width:"500px", height:"500px"}}></canvas>  
+        
+        <canvas></canvas>
+        // <img src={map} alt="Girl in a jacket" width="600" height="600"/>
+    )
+}
+export default FlightCanvas
