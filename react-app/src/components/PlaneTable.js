4d3ee695aa6bf304f3bc33a30c79bcb7b4f5aa68
Jeff Castro <116311056+Jcastro197@users.noreply.github.com>
2023-02-19 12:03:51 -0500

    ""

--- react-app/src/components/PlaneTable.js
+++ react-app/src/components/PlaneTable.js
@@ -3,20 +3,7 @@ import React from 'react'
 const PlaneTable = () => {
   return (
     <div className='plane-table'>
-      <div className='plane-table__header'>
-        <div className='plane-table__header__item'>Plane</div>
-        <div className='plane-table__header__item'>Origin</div>
-        <div className='plane-table__header__item'>Destination</div>
-        <div className='plane-table__header__item'>Departure</div>
-        <div className='plane-table__header__item'>Arrival</div>
-      </div>
-      <div className='plane-table__body'>
-        <div className='plane-table__body__item'>Plane</div>
-        <div className='plane-table__body__item'>Origin</div>
-        <div className='plane-table__body__item'>Destination</div>
-        <div className='plane-table__body__item'>Departure</div>
-        <div className='plane-table__body__item'>Arrival</div>
-      </div>
+     
     </div>
   )
 }
